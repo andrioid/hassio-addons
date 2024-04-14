3d4273732cd8f30b7bbae8a6073feba8e6a6e340
Andri Óskarsson <account+github@andri.dk>
2024-04-14 12:47:25 +0000

    wip

--- smart-house-buttons/run.sh
+++ smart-house-buttons/run.sh
@@ -7,9 +7,13 @@ host=$(bashio::config 'server.host')
 
 bashio::log.info "Reverse tunnel configured for $username@$host"
 
+bashio::log.info "Setting ENV variables"
+
 MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 SH_HOST=$(bashio::config 'modbus_host')
+
+bashio::log.info "Running app"
 
 bun index.ts
